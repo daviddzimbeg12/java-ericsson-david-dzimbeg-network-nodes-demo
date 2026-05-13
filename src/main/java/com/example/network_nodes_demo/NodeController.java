fb3946eb3ed2be1eb6b2593678e8c728b841f6de
David Dzimbeg <david.dzimbeg12@gmail.com>
2026-05-13 11:50:55 +0200

    zadatak-7 update

--- src/main/java/com/example/network_nodes_demo/NodeController.java
+++ src/main/java/com/example/network_nodes_demo/NodeController.java
@@ -1,7 +1,10 @@
 package com.example.network_nodes_demo;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
